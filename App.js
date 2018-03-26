3dce038c4b924ddf530068957a0085820e2b3949
Frederick Akalin <akalin@keyba.se>
2018-03-26 16:24:59 -0700

    Add selection handling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Button, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,13 +8,29 @@ export default class App extends React.Component {
   }
 
   _onChangeText = (text) => {
+    console.log('text changed to ', text)
+    this.setState({text})
+  }
+
+  _onSelectionChange = (event) => {
+    console.log('selection changed to ', event.nativeEvent.selection)
+    this.setState({
+      selection: event.nativeEvent.selection,
+    })
+  }
+
+  _onButtonPress = () => {
+    let {text, selection} = this.state
+    selection = selection || {start: 0, end: 0}
+    text = text.substring(0, selection.start) + "foo" + text.substring(selection.end)
     this.setState({text})
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={{height:40, width: 150, borderColor: 'black', borderWidth: 1}} onChangeText={this._onChangeText} value={this.state.text} />
+        <TextInput style={{height:40, width: 150, borderColor: 'black', borderWidth: 1}} onChangeText={this._onChangeText} onSelectionChange={this._onSelectionChange} value={this.state.text} />
+        <Button onPress={this._onButtonPress} title="Replace selection with foo" />
       </View>
     );
   }
